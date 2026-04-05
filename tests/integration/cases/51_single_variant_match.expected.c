eda5eedd0310969b32fbc717361e1434660e6a2b
Alex Turner <alexturner@meta.com>
2026-04-05 14:19:52 -0700

    Add test: single-variant phc_descr with match + destructuring
    Tests the degenerate case: phc_descr with only one variant.
    Exhaustiveness check, match, and destructuring all work correctly.

---
+++ tests/integration/cases/51_single_variant_match.expected.c
@@ -0,0 +1,43 @@
+extern void abort(void);
+#include <stdio.h>
+
+typedef enum {
+    Wrapper_Value,
+    Wrapper__COUNT
+} Wrapper_Tag;
+
+typedef struct {
+    int x;
+} Wrapper_Value_t;
+
+typedef struct Wrapper {
+    Wrapper_Tag tag;
+    union {
+        Wrapper_Value_t Value;
+    };
+} Wrapper;
+
+static inline Wrapper Wrapper_mk_Value(int x) {
+    Wrapper _v;
+    _v.tag = Wrapper_Value;
+    _v.Value.x = x;
+    return _v;
+}
+
+static inline Wrapper_Value_t Wrapper_as_Value(Wrapper v) {
+    if (v.tag != Wrapper_Value) abort();
+    return v.Value;
+}
+#line 6
+
+int main(void) {
+    Wrapper w = Wrapper_mk_Value(42);
+    switch (w.tag) {
+    case Wrapper_Value: { int x = w.Value.x; {
+            printf("value: %d\n", x);
+        } break; }
+    default: break;
+}
+#line 14
+    return 0;
+}
